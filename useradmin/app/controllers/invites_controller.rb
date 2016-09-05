33f335bbae733a5a527c46cd46f5510e0372ccb2
Stephan van Diepen <svdiepen@zilverline.com>
2016-09-05 14:15:32 +0200

    Templating with slim-lang http://slim-lang.com/

--- useradmin/app/controllers/invites_controller.rb
+++ useradmin/app/controllers/invites_controller.rb
@@ -1,74 +1,54 @@
 class InvitesController < ApplicationController
-  before_action :set_invite, only: [:show, :edit, :update, :destroy]
-
-  # GET /invites
-  # GET /invites.json
   def index
-    @invites = Invite.all
+    @invites = invites
   end
 
-  # GET /invites/1
-  # GET /invites/1.json
   def show
+    @invite = invite
   end
 
-  # GET /invites/new
   def new
     @invite = Invite.new
   end
 
-  # GET /invites/1/edit
   def edit
+    @invite = invite
   end
 
-  # POST /invites
-  # POST /invites.json
   def create
     @invite = Invite.new(invite_params)
 
-    respond_to do |format|
-      if @invite.save
-        format.html { redirect_to @invite, notice: 'Invite was successfully created.' }
-        format.json { render :show, status: :created, location: @invite }
-      else
-        format.html { render :new }
-        format.json { render json: @invite.errors, status: :unprocessable_entity }
-      end
+    if @invite.save
+      redirect_to @invite, notice: 'Invite was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /invites/1
-  # PATCH/PUT /invites/1.json
   def update
-    respond_to do |format|
-      if @invite.update(invite_params)
-        format.html { redirect_to @invite, notice: 'Invite was successfully updated.' }
-        format.json { render :show, status: :ok, location: @invite }
-      else
-        format.html { render :edit }
-        format.json { render json: @invite.errors, status: :unprocessable_entity }
-      end
+    if @invite.update(invite_params)
+      redirect_to @invite, notice: 'Invite was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /invites/1
-  # DELETE /invites/1.json
   def destroy
     @invite.destroy
-    respond_to do |format|
-      format.html { redirect_to invites_url, notice: 'Invite was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to invites_url, notice: 'Invite was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_invite
-      @invite = Invite.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def invite_params
-      params.require(:invite).permit(:email, :token, :accepted_by, :accepted_at)
-    end
+  def invite
+    Invite.find(params[:id])
+  end
+
+  def invites
+    Invite.all
+  end
+
+  def invite_params
+    params.require(:invite).permit(:email, :token, :accepted_by, :accepted_at)
+  end
 end
