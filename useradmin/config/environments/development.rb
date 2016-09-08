5d381e5c57616e355ef8c50ec77e2164383f9ea3
Bob Forma <bobforma@gmail.com>
2016-09-08 17:18:08 +0200

    Install & configure MailCatcher

--- useradmin/config/environments/development.rb
+++ useradmin/config/environments/development.rb
@@ -39,9 +39,13 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = {
     host: '192.168.111.171'
+  }
+  config.action_mailer.smtp_settings = {
+    address: '192.168.111.171',
+    port: 1025
   }
 
   config.one_client = ActiveSupport::OrderedOptions.new
