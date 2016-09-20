5a476e573d25a97ffa866b22385e03893140dec3
Stephan van Diepen <svdiepen@zilverline.com>
2016-09-20 14:28:51 +0200

    current_user#group_admin? spec

---
+++ useradmin/spec/lib/current_user_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe CurrentUser, :vcr do
+  let(:current_user) { CurrentUser.new(double({})) }
+
+  describe '#group_admin?' do
+    subject { current_user.group_admin? }
+
+    context 'when not a surfsara_admin' do
+      before { allow(current_user).to receive(:surfsara_admin?).and_return(false) }
+
+      context 'with groups' do
+        before { allow(current_user).to receive(:admin_groups).and_return([double({})]) }
+        it { is_expected.to be_truthy }
+      end
+
+      context 'without groups' do
+        before { allow(current_user).to receive(:admin_groups).and_return([]) }
+        it { is_expected.to be_falsey }
+      end
+    end
+
+    context 'when a surfsara_admin' do
+      before { allow(current_user).to receive(:surfsara_admin?).and_return(true) }
+      it { is_expected.to be_falsey }
+    end
+  end
+end
