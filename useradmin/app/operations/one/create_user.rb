9180656b8d15f73d3d4d9320d3bff1113c70c1cf
Bob Forma <bobforma@gmail.com>
2016-09-21 15:13:55 +0200

    Refactor One::Create to reduce complexity
    And increase readability.

--- useradmin/app/operations/one/create_user.rb
+++ useradmin/app/operations/one/create_user.rb
@@ -1,20 +1,43 @@
 module One
   class CreateUser < Operation
-    def process(params)
-      create_or_update_user(params.fetch(:current_user), params.fetch(:invite))
+    def process(_params)
+      add_user_to_group unless user_already_member_of_group?
+      add_user_as_group_admin if become_group_admin? && !user_already_admin_of_group?
     end
 
     private
 
-    def create_or_update_user(current_user, invite)
-      user = OneClient.find_user(current_user.one_username)
-      user = OneClient.create_user(current_user.one_username, current_user.one_password) if user.nil?
+    def invite
+      @params.fetch(:invite)
+    end
 
-      OneClient.add_user_to_group(user.id, invite.group_id) unless user.group_ids.include?(invite.group_id)
+    def one_user
+      @one_user ||= find_or_create_user
+    end
 
-      if invite.role == Role.group_admin && !OneClient.user_admin_of_group?(user.id, invite.group_id)
-        OneClient.make_user_group_admin(user.id, invite.group_id)
-      end
+    def find_or_create_user
+      OneClient.find_user(current_user.one_username) ||
+        OneClient.create_user(current_user.one_username, current_user.one_password)
+    end
+
+    def add_user_to_group
+      OneClient.add_user_to_group(one_user.id, invite.group_id)
+    end
+
+    def add_user_as_group_admin
+      OneClient.make_user_group_admin(one_user.id, invite.group_id)
+    end
+
+    def user_already_member_of_group?
+      one_user.group_ids.include?(invite.group_id)
+    end
+
+    def become_group_admin?
+      invite.role == Role.group_admin
+    end
+
+    def user_already_admin_of_group?
+      OneClient.user_admin_of_group?(one_user.id, invite.group_id)
     end
   end
 end
