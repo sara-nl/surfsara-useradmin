a4ee581f2c3db8f58b1531060fb1fb2db4410e7a
Bob Forma <bobforma@gmail.com>
2016-09-21 14:41:04 +0200

    Show sane message when not deploying

--- useradmin/deploy.sh
+++ useradmin/deploy.sh
@@ -3,6 +3,6 @@ set -ex
 
 git checkout master
 git pull -r
-rspec spec/
-rubocop
+rspec spec/ || (echo 'Not deploying due to failed specs!'; exit 1)
+rubocop || (echo 'Not deploying due to style violations!'; exit 1)
 ansible-playbook ../servers/deploy.yml -i ../servers/inv/acceptance
