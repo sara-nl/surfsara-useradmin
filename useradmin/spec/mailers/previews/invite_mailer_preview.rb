0d69d5e65bbdcaef299835c32b07c651ab48f2c1
Stephan van Diepen <svdiepen@zilverline.com>
2016-09-26 15:28:16 +0200

    Translate role in mailer

--- useradmin/spec/mailers/previews/invite_mailer_preview.rb
+++ useradmin/spec/mailers/previews/invite_mailer_preview.rb
@@ -1,7 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/invite_mailer
 class InviteMailerPreview < ActionMailer::Preview
   def invitation
-    invite = Invite.new(email: 'john.doe@example.com', group_name: 'testgroup', role: 'admin')
+    invite = Invite.new(email: 'john.doe@example.com', group_name: 'testgroup', role: Role.group_admin)
     InviteMailer.invitation(invite, 'eb693ec8252cd630102fd0d0fb7c3485')
   end
 end
