3958836417c3c9f3b7e94703f0527c75dc97688b
Bob Forma <bobforma@gmail.com>
2016-09-26 16:34:06 +0200

    Set invite mail from header to sender
    Use the primary email address from the Shibboleth mail header.

--- useradmin/spec/factories/current_user.rb
+++ useradmin/spec/factories/current_user.rb
@@ -4,5 +4,8 @@ FactoryGirl.define do
     one_username 'isaac@university-example.org'
     one_password '3d21c8a6d89e3332e3d388852f99d5e5ce114ff9'
     admin_groups { build_list(:one_group, 1) }
+    common_name 'Sir Isaac Newton'
+    email 'isaacnewton@university-example.org;newton@university-example.org'
+    primary_email_address 'isaacnewton@university-example.org'
   end
 end
