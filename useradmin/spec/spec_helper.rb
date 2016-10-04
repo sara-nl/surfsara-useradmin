d426ec18a6b580d2bb73641cdcc12d2212710d3d
Bob Forma <bobforma@gmail.com>
2016-10-04 11:17:25 +0200

    Allow localhost requests with VCR
    For capybara.

--- useradmin/spec/spec_helper.rb
+++ useradmin/spec/spec_helper.rb
@@ -14,6 +14,7 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.allow_http_connections_when_no_cassette = false
+  config.ignore_localhost = true
 end
 
 RSpec.configure do |config|
